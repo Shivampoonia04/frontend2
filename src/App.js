b453bc4c2f1d47ce72937f0a20f031c86ec6f568
Shivampoonia04 <shivam1309.be21@chitkara.edu.in>
2024-09-15 15:16:18 +0530

    Fix routing issues with react-router-dom v6

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, BrowserRouter as Router } from 'react-router-dom';
-import { Route, Switch } from 'react-router-dom';
+import { Route} from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
 import ChatbotPage from './pages/ChatbotPage';
